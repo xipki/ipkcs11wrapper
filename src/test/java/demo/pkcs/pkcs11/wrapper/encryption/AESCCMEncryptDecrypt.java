1b1cf914e6df1348fc17df70a814ab5392df3dbc
Lijun Liao <lijun.liao@gmail.com>
2023-01-05 18:23:02 +0100

    add CCM test

---
+++ src/test/java/demo/pkcs/pkcs11/wrapper/encryption/AESCCMEncryptDecrypt.java
@@ -0,0 +1,78 @@
+/*
+ *
+ * Copyright (c) 2019 Lijun Liao
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package demo.pkcs.pkcs11.wrapper.encryption;
+
+import demo.pkcs.pkcs11.wrapper.util.Util;
+import org.junit.Test;
+import org.xipki.pkcs11.Mechanism;
+import org.xipki.pkcs11.Token;
+import org.xipki.pkcs11.TokenException;
+import org.xipki.pkcs11.objects.AttributeVector;
+import org.xipki.pkcs11.parameters.CcmParameters;
+import org.xipki.pkcs11.parameters.GcmParameters;
+
+import static org.xipki.pkcs11.PKCS11Constants.*;
+
+/**
+ * This demo program uses a PKCS#11 module to encrypt and decrypt via
+ * CKM_AES_GCM.
+ *
+ * @author Lijun Liao
+ */
+public class AESCCMEncryptDecrypt extends SymmEncryptDecrypt {
+
+  private final byte[] iv;
+
+  private final byte[] aad;
+
+  public AESCCMEncryptDecrypt() {
+    iv = randomBytes(12);
+    aad = new byte[20];
+    // aad = "hello".getBytes();
+  }
+
+  @Test
+  @Override
+  public void main() throws TokenException {
+    if (!Util.supports(getNonNullToken(), CKM_AES_CCM)) {
+      System.err.println("AES-CCM unsupported in the HSM, skip");
+      return;
+    }
+
+    super.main();
+  }
+
+  @Override
+  protected Mechanism getKeyGenMech(Token token) throws TokenException {
+    return getSupportedMechanism(token, CKM_AES_KEY_GEN);
+  }
+
+  @Override
+  protected Mechanism getEncryptionMech(Token token) throws TokenException {
+    Mechanism mech = getSupportedMechanism(token, CKM_AES_CCM);
+    CcmParameters params = new CcmParameters(0, iv, aad, 128);
+    mech.setParameters(params);
+    return mech;
+  }
+
+  @Override
+  protected AttributeVector getKeyTemplate() {
+    return newSecretKey(CKK_AES).encrypt(true).decrypt(true).valueLen(16);
+  }
+
+}
